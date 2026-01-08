f92ef7c3e4f597384addb1fdccbdc497966187ee
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-08 11:08:57 +0800

    feat(06_repeatString): add solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    let res = []
+    if (n < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < n; i++){
+        res.push(str)
+    }
+    return res.join('')
 };
 
 // Do not edit below this line
