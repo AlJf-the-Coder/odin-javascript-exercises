dccaeda848e2aa0510bb5e4bac873f47770ef402
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-11 00:02:43 +0800

    feat(13_palindromes): add solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanString = str.split('')
+                          .filter(c => {
+                            // check if alphanumeric
+                            const code = c.charCodeAt(0);
+                            return (code > 47 && code < 58) ||
+                                (code > 64 && code < 91) ||
+                                (code > 96 && code < 123);
+                          })
+                          .map(c => c.toLowerCase())
+    let l = 0,
+        r = cleanString.length - 1;
+    while (l < r){
+        if (cleanString[l] != cleanString[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
