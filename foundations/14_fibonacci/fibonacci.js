551f716174d12232bec77f2ab811c5f80542b068
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-11 00:28:11 +0800

    feat(14_fibonacci): add solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
+
+    if (typeof n != "number" &&
+        (n === '' || typeof n != "string")){
+        return 'OOPS'
+    }
+    n = +n;
+    
+    if (!Number.isInteger(n)) return;
+
+    const fib = [0, 1];
+    while (fib.length <= n){
+        fib.push(fib.at(-1) + fib.at(-2));
+    }
+    return fib[n];
 
 };
 
