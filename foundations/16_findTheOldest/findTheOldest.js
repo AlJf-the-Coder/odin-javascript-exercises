da12821307f81102af6776e1137b9cf98f2657f8
AlJf-the-Coder <96684607+AlJf-the-Coder@users.noreply.github.com>
2026-01-11 00:55:24 +0800

    feat(16_findTheOldest): add solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const lastYearOfDeath = people.map(person => person.yearOfDeath).filter(year => !isNaN(year)).reduce((lastYr, yr) => Math.max(lastYr, yr));
+    return people.reduce((oldest, person) => {
+        const oldestAge = (oldest.yearOfDeath ?? lastYearOfDeath) - oldest.yearOfBirth;
+        const personAge = (person.yearOfDeath ?? lastYearOfDeath) - person.yearOfBirth;
+        console.log(oldest.name, oldestAge, person.name, personAge);
+        return personAge > oldestAge ? person: oldest;
+    })
 };
 
 // Do not edit below this line
